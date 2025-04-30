2b757881734f293e8f144e02bc43fc27f4211a67
TenixOue <79940202+TenixOue@users.noreply.github.com>
2025-04-30 14:49:47 +0200

    periode

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
+import math
+
 import numpy as np
-g = 9.8 #𝑔 = 9.8 𝑚. 𝑠 ^−2
-l = 1.0 #𝑙 = 1.0 𝑚
+
+g = 9.8  # 𝑔 = 9.8 𝑚. 𝑠 ^−2
+l = 1.0  # 𝑙 = 1.0 𝑚
+
 
 def subdiv_reg(a, b, n):
-
     h = (b - a) / n
-    return [ a + k * h for k in range(n + 1) ]
+    return [a + k * h for k in range(n + 1)]
+
 
 def f(x):
-    return x**2
+    return x ** 2
 
-def int_rectangle(f : callable([[float], float]), sub_div_list):
+
+def int_rectangle(f: callable([[float], float]), sub_div_list):
     integral = 0
     N = len(sub_div_list) - 1
     for i in range(N):
         integral += f(sub_div_list[i])
     return integral * (sub_div_list[1] - sub_div_list[0])
 
-def int_trapez(f : callable([[float], float]), sub_div_list):
+
+def int_trapez(f: callable([[float], float]), sub_div_list):
     integral = 0
     N = len(sub_div_list) - 1
     for i in range(N):
-        integral += (f(sub_div_list[i]) + f(sub_div_list[i+1])) / 2
+        integral += (f(sub_div_list[i]) + f(sub_div_list[i + 1])) / 2
     return integral * (sub_div_list[1] - sub_div_list[0])
 
-def f2(x):
-    return np.sqrt(1 - x**2)
 
-if(int(input("Type 1 to show 𝐼 = ∫₀¹ √(1 − x²) dx = π/4 \n")) == 1):
+def f2(x):
+    return np.sqrt(1 - x ** 2)
+
+
+def periode(theta_max):
+    """
+    T(θₘₐₓ) = 2 √2ℓ/g ∫₀^{θₘₐₓ} dθ / √(cos θ − cos θₘₐₓ)
+    """
+    integral = lambda theta: 1 / np.sqrt(np.cos(theta) - np.cos(theta_max))
+    res = 2 * math.sqrt(2 * l / g) * int_rectangle(integral, subdiv_reg(0, theta_max, 100))
+    return res
+
+
+inputVal = int(input("Type 1 to show 𝐼 = ∫₀¹ √(1 − x²) dx = π/4 \n Type 2 to show T(θₘₐₓ) = 2 √2ℓ/g ∫₀^{θₘₐₓ} dθ / √(cos θ − cos θₘₐₓ) \n"))
+if (inputVal == 1):
     # Integral of f(x) from 0 to 1 using rectangles and trapez
     rec100 = int_rectangle(f2, subdiv_reg(0, 1, 100))
     rec1000 = int_rectangle(f2, subdiv_reg(0, 1, 1000))
@@ -43,3 +61,6 @@ if(int(input("Type 1 to show 𝐼 = ∫₀¹ √(1 − x²) dx = π/4 \n")) == 1
     print("Integral of f2(x) from 0 to 1 using trapez1000: ", trap1000)
     print("Integral of f2(x) from 0 to 1 using trapez10000: ", trap10000)
 
+if (inputVal == 2):
+    vl = periode(math.pi / 4)
+    print("T(π/2) = ", vl)
